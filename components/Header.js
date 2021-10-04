dc956d5ad038dd931d9e117b1e3fbf2e4ce7e073
zrincin <z.zrakoplovac@gmail.com>
2021-10-04 20:32:16 +0200

    change UI header

--- components/Header.js
+++ components/Header.js
@@ -6,13 +6,8 @@ const Header = () => {
   return (
     <Menu style={{ marginTop: "10px" }}>
       <Link route="/">
-        <a className="item"><Icon name="home"/>Home</a>
+        <a className="item"><Icon name="home"/><b>Campaigns</b></a>
       </Link>
-      <Menu.Menu>
-        <Link route="/">
-          <a className="item">Campaigns</a>
-        </Link>
-      </Menu.Menu>
       <Menu.Menu>
         <Link route="/campaigns/new">
           <a className="item"><Icon name="plus"/></a>
